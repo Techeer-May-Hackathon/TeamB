5c10262d0e777b6aebf0f154aa1c5f75722a6bda
0BVer <dr0joon@gmail.com>
2022-05-07 19:43:22 +0900

    feat : OrderMapper 테스트 추가

---
+++ src/test/java/com/techeer/svproject/domain/order/controller/OrderControllerTest.java
@@ -0,0 +1,24 @@
+package com.techeer.svproject.domain.order.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getDetail() {
+    }
+
+    @Test
+    void getByUserId() {
+    }
+
+    @Test
+    void deleteOrder() {
+    }
+}
