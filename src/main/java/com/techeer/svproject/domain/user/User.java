dd9b77328cf04641f4193f7f36c0238a975cd1a9
choiseyeon <choi20014830@gmail.com>
2022-05-08 04:00:24 +0900

    Feat: UserRepository 테스트 코드 추가

--- src/main/java/com/techeer/svproject/domain/user/User.java
+++ src/main/java/com/techeer/svproject/domain/user/User.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Setter
 @Table(name = "account")
--- src/main/java/com/techeer/svproject/domain/user/UserRepository.java
+++ src/main/java/com/techeer/svproject/domain/user/UserRepository.java
@@ -2,7 +2,6 @@ package com.techeer.svproject.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
