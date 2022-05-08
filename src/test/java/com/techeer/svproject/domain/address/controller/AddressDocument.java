3270797ce0d2c1e41dbb62cc016e4815207e16db
kim1387 <kevink0@naver.com>
2022-05-08 13:18:13 +0900

    feat: add update address api test

--- src/test/java/com/techeer/svproject/domain/address/controller/AddressDocument.java
+++ src/test/java/com/techeer/svproject/domain/address/controller/AddressDocument.java
@@ -5,8 +5,7 @@ import org.springframework.restdocs.payload.JsonFieldType;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 
 public class AddressDocument {
     public static RestDocumentationResultHandler getAddress() {
@@ -14,6 +13,21 @@ public class AddressDocument {
         return document("api/v1/address-list",
                 requestParameters(
                     parameterWithName("userEmail").description("The page to retrieve")
+                ),
+                responseFields(
+                        fieldWithPath("addressId").type(JsonFieldType.STRING).description("addressId"),
+                        fieldWithPath("state").type(JsonFieldType.STRING).description("state"),
+                        fieldWithPath("city").type(JsonFieldType.STRING).description("city"),
+                        fieldWithPath("street").type(JsonFieldType.STRING).description("street"),
+                        fieldWithPath("zipcode").type(JsonFieldType.NUMBER).description("zip code")
+                )
+        );
+    }
+    public static RestDocumentationResultHandler updateAddress() {
+
+        return document("/address-list/{address-id}",
+                pathParameters(
+                        parameterWithName("address-id").description("수정될 address Id")
                 ),
                 responseFields(
                         fieldWithPath("addressId").type(JsonFieldType.STRING).description("addressId"),
